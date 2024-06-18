acf74affc4a5b64be464f3b625ee63926a21f220
Jason Whitehouse <jasonwhitehouse@live.co.uk>
2024-06-18 18:34:55 +0000

    updating file location

--- coffee_shop_jasonw/models/monthly_users.sql
+++ coffee_shop_jasonw/models/monthly_users.sql
@@ -2,6 +2,6 @@ select
     date_trunc(first_order_at, month) as signup_month,
     count(*) as new_customers
  
-from `aec-students.dbt-jasonw.customers`
+from `aec-students.dbt_jasonw.customers`
  
 group by 1
