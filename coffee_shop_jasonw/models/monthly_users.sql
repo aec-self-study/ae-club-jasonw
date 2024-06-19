5b423130c58b99227d6244435664281978de6810
Jason Whitehouse <jasonwhitehouse@live.co.uk>
2024-06-19 17:25:58 +0000

    dim_customers.yml

--- coffee_shop_jasonw/models/monthly_users.sql
+++ coffee_shop_jasonw/models/monthly_users.sql
@@ -4,6 +4,6 @@ select
     date_trunc(first_order_at, month) as signup_month,
     count(*) as new_customers
  
-from `aec-students.dbt_jasonw.customers`
+from {{ ref('dim_customers') }}
  
 group by 1
