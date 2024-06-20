61670c68eaea7baf2464768937724703a0fb84cd
Jason Whitehouse <jasonwhitehouse@live.co.uk>
2024-06-20 18:49:51 +0000

    adding dynamic sources, and doc block to dim_customers description

--- coffee_shop_jasonw/models/staging/stg_coffee_shop__customer_orders.sql
+++ coffee_shop_jasonw/models/staging/stg_coffee_shop__customer_orders.sql
@@ -6,5 +6,5 @@ select
      , min(created_at) as first_order_at
      , max(created_at) as latest_order_at
      ,sum(total) as total_spend
-from {{ source('src_coffee_shop') }} --`analytics-engineers-club.coffee_shop.orders` 
+from {{ source('coffee_shop', 'orders') }} --`analytics-engineers-club.coffee_shop.orders` 
 group by 1
