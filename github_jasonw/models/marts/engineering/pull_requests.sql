9cc5d7d1b1c0774def7d4666c3cbc4090195ac16
Jason Whitehouse <jasonwhitehouse@live.co.uk>
2024-06-24 14:21:17 +0000

    first marts table created

---
+++ github_jasonw/models/marts/engineering/pull_requests.sql
@@ -0,0 +1,50 @@
+with
+    pull_request as (
+        select * from {{ ref('stg_github__pull_request') }}
+    ),
+
+    repositories as (
+        select * from {{ ref('stg_github__repositories') }}
+    ),
+
+    issue as (
+        select * from {{ ref('stg_github__issue') }}
+    ),
+
+    issues_merged as (
+        select * from {{ ref('stg_github__issues_merged') }}
+    ),
+
+
+final as (
+
+select
+    pull_request.pull_request_id,
+    repositories.name as repo_name,
+    issue.number as pull_request_number,
+    --TO DO: Find out how to label this
+    cast(null as string) as type, --(bug, eng, features)
+    --TO DO: Find out how to label this
+    case
+        when pull_request.is_draft then 'draft'
+        when issues_merged.merged_at is not null then 'merged'
+        when issue.closed_at is not null then 'closed_without_merge'
+        else 'open'
+    end as state,
+
+
+    issue.created_at as opened_at,
+    issues_merged.merged_at,
+    date_diff(issue.created_at, issues_merged.merged_at, hour) / 24.0 as days_open_to_merge
+
+from pull_request
+left join repositories
+    on pull_request.head_repo_id = repositories.repo_id
+left join issue
+    on pull_request.issue_id = issue.issue_id
+left join issues_merged
+    on pull_request.issue_id = issues_merged.issue_id
+
+)
+
+select * from final
